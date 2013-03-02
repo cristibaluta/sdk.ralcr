6b94e5ad3e26f0d09e467e16de85efaeb75b24e4
Baluta Cristian <cristi.baluta@gmail.com>
2013-03-02 12:34:25 +0000

    Added RCWebView. Work only on nme and is using native extensions. It has a lazy init useful for modalviewcontroller performance

---
+++ UIKit/RCWebView.hx
@@ -0,0 +1,51 @@
+
+class RCWebView {
+	
+	// Replicate the NMEWebView events
+	public var didFinishLoad :RCSignal<String->Void>;
+	public var didFinishWithError :RCSignal<String->Void>;
+	
+	#if nme
+	var webView :NMEWebView;
+	#end
+	var x :Float;
+	var y :Float;
+	var w :Float;
+	var h :Float;
+	var url :String;
+	
+	public function new (x:Float, y:Float, w:Float, h:Float, url:String) {
+		
+		didFinishLoad = new RCSignal<String->Void>();
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		this.url = url;
+		
+	}
+	
+	public function init () {
+		#if nme
+		webView = new NMEWebView (x, y, w, h, url);
+		webView.didFinishLoad.add ( webViewDidFinishLoad );
+		#end
+	}
+	
+	function webViewDidFinishLoad (url:String) :Void {
+		trace(didFinishLoad);
+		didFinishLoad.dispatch ( url );
+	}
+    
+	public function destroy() :Void {
+		trace("destroy web view");trace(didFinishLoad);
+		#if nme
+		Fugu.safeDestroy ( webView );
+		webView = null;
+		#end
+		if (didFinishLoad != null) {
+			didFinishLoad.destroy();
+			didFinishLoad = null;
+		}
+	}
+}
